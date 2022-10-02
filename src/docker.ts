58a226ecd08249c4bc21b04d8452e8b661585575
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-10-02 11:41:10 +0300

    Add docker related code

---
+++ src/docker.ts
@@ -0,0 +1,31 @@
+import * as exec from '@actions/exec'
+
+/**
+ * Pulls a docker image
+ * @param {string} dockerImage The name of the image to be pulled
+ */
+export async function pullDockerImage(dockerImage: string): Promise<number>
+{
+    return exec.exec(`docker pull ${dockerImage}`)
+}
+
+/**
+ * Creates and returns the docker command to be executed
+ *
+ * @param {string} dockerImage The docker image to be run.
+ * @param {string} configDir The directory containing the configuration files of the automation framework.
+ * @param {string} reportsDir The directory containing the reports created by the automation framework.
+ * @param {string} autorunFile The actual automation framework configuration
+ * @return {string} The docker command to be run
+ */
+export function getDockerCommand(dockerImage: string, configDir: string, reportsDir: string, autorunFile: string): string
+{
+    const workspace: string = process.env.GITHUB_WORKSPACE ?? ''
+    const bashCmd = `/zap/zap.sh -cmd -addonupdate; /zap/zap.sh -cmd -autorun /zap/${configDir}/${autorunFile}`
+
+    let dockerCmd = `docker run -v ${workspace}/${configDir}/:/zap/${configDir}/:rw `
+    dockerCmd += `-v ${workspace}/${reportsDir}:/zap/${reportsDir}/:rw `
+    dockerCmd += `--network="host" -t ${dockerImage} /bin/bash -c '${bashCmd}'`
+
+    return dockerCmd
+}
