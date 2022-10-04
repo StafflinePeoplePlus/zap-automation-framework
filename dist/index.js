bfee5c908a8dd5f69332e1e9db9ecc6fd41c1940
Alexandros Koutroulis <icyd3mon@gmail.com>
2022-10-04 21:17:58 +0300

    Bash issue

--- dist/index.js
+++ dist/index.js
@@ -139,7 +139,7 @@ exports.pullDockerImage = pullDockerImage;
 function getDockerCommand(dockerImage, configDir, reportsDir, autorunFile) {
     var _a;
     const workspace = (_a = process.env.GITHUB_WORKSPACE) !== null && _a !== void 0 ? _a : '';
-    const bashCmd = `/bin/bash -c 'mkdir reports; /zap/zap.sh -cmd -autorun /zap/${configDir}/${autorunFile}'`;
+    const bashCmd = `mkdir reports && /zap/zap.sh -cmd -autorun /zap/${configDir}/${autorunFile}'`;
     let dockerCmd = `docker run --mount type=bind,source=${workspace}/${configDir},target=/zap/${configDir} `;
     dockerCmd += `--mount type=bind,source=${reportsDir},target=/zap/reports `;
     dockerCmd += `--network="host" -t ${dockerImage} ${bashCmd}`;
